def7abd22f94532ea62f96a3538e3d1421bc80d2
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-24 21:22:11 -0800

    feature/presence-status: add config settings cmd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 // import chalk from 'chalk'
 import presenceCommand from './presence'
+import configCommand from './config-cmd'
+console.log('configCommand = ', configCommand)
 
 import { command } from './args'
 
 switch (command) {
   case 'presence': {
     presenceCommand()
+    break
+  }
+  case 'setup': {
+    configCommand()
     break
   }
   default:
